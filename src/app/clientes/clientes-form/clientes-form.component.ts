205f9e6a07ef05e2e947fa01dc5dbb68a3291d36
“charlesdccti” <charlesdccti@gmail.com>
2022-07-25 17:21:58 -0300

    Created the Customers form

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../Cliente';
 
 @Component({
   selector: 'app-clientes-form',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class ClientesFormComponent implements OnInit {
+
+  cliente: Cliente; 
 
   constructor() { }
 
