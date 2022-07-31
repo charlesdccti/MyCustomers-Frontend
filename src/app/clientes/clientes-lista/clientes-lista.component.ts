c04004434429079a197dde5f54dd0bcef8ff7ce8
“charlesdccti” <charlesdccti@gmail.com>
2022-07-31 17:34:16 -0300

    Add modal for confirmation of deletion

--- src/app/clientes/clientes-lista/clientes-lista.component.ts
+++ src/app/clientes/clientes-lista/clientes-lista.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { ClientesService } from 'src/app/clientes.service';
+import { Router } from '@angular/router'
+
+import { ClientesService } from '../../clientes.service'
 import { Cliente } from '../Cliente';
+
 
 @Component({
   selector: 'app-clientes-lista',
@@ -11,19 +13,37 @@ import { Cliente } from '../Cliente';
 export class ClientesListaComponent implements OnInit {
 
   clientes: Cliente[] = [];
+  clienteSelecionado: Cliente;
+  mensagemSucesso: string;
+  mensagemErro: string;
 
   constructor(
     private service: ClientesService, 
     private router: Router) {}
 
-  ngOnInit(): void {    
+  ngOnInit(): void {
     this.service
       .getClientes()
-      .subscribe( response => this.clientes = response );
+      .subscribe( resposta => this.clientes = resposta );
   }
 
   novoCadastro(){
     this.router.navigate(['/clientes-form'])
   }
-  
+
+  preparaDelecao(cliente: Cliente){
+    this.clienteSelecionado = cliente;
+  }
+
+  deletarCliente(){
+    this.service
+      .deletar(this.clienteSelecionado)
+      .subscribe( 
+        response => {
+          this.mensagemSucesso = 'Cliente deletado com sucesso!'
+          this.ngOnInit();
+        },
+        erro => this.mensagemErro = 'Ocorreu um erro ao deletar o cliente.'  
+      )
+  }
 }
