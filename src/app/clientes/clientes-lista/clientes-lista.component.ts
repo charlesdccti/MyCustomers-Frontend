a7bad970421f84a17fb295d1ae32c7fd0ac74a4d
“charlesdccti” <charlesdccti@gmail.com>
2022-08-01 18:01:24 -0300

    update in modal for confirmation of deletion

--- src/app/clientes/clientes-lista/clientes-lista.component.ts
+++ src/app/clientes/clientes-lista/clientes-lista.component.ts
@@ -31,19 +31,22 @@ export class ClientesListaComponent implements OnInit {
     this.router.navigate(['/clientes-form'])
   }
 
+
   preparaDelecao(cliente: Cliente){
     this.clienteSelecionado = cliente;
   }
+
 
   deletarCliente(){
     this.service
       .deletar(this.clienteSelecionado)
       .subscribe( 
         response => {
-          this.mensagemSucesso = 'Cliente deletado com sucesso!'
-          this.ngOnInit();
-        },
-        erro => this.mensagemErro = 'Ocorreu um erro ao deletar o cliente.'  
+        this.mensagemSucesso = 'Cliente deletado com sucesso!'
+        this.ngOnInit();
+      },
+        erro => this.mensagemErro = 'Ocorreu um erro ao deletar o cliente.'
       )
   }
+
 }
